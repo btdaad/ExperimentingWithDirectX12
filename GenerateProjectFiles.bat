78cb5c6328a7299fd71621107920a25834025d3c
btdaad <axelle.xilou@gmail.com>
2026-02-13 15:57:44 +0100

    :wrench: Fix GenerateProjectFiles and change CMakeLists project name

--- GenerateProjectFiles.bat
+++ GenerateProjectFiles.bat
@@ -1,21 +1,20 @@
 @ECHO OFF
-
-PUSHD %~dp0
+PUSHD "%~dp0"
 
 REM Update these lines if the currently installed version of Visual Studio is not 2022.
-SET CMAKE="cmake.exe"
-SET CMAKE_GENERATOR="Visual Studio 17 2022"
-SET CMAKE_BINARY_DIR=build_vs2022
+set "CMAKE=cmake.exe"
+set "CMAKE_GENERATOR=Visual Studio 17 2022"
+set "CMAKE_BINARY_DIR=build_vs2022"
 
-MKDIR %CMAKE_BINARY_DIR% 2>NUL
-PUSHD %CMAKE_BINARY_DIR%
+MKDIR "%CMAKE_BINARY_DIR%" 2>NUL
+PUSHD "%CMAKE_BINARY_DIR%"
 
-%CMAKE% -G %CMAKE_GENERATOR% -Wno-dev "%~dp0"
+"%CMAKE%" -G "%CMAKE_GENERATOR%" -Wno-dev "%~dp0."
 
 IF ERRORLEVEL 1 (
     PAUSE
 ) ELSE (
-    START LearningDirectX12.sln
+    START "" "ExperimentingWithDirectX12.sln"
 )
 
 POPD
